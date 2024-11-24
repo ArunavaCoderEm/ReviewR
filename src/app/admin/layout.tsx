07df43e5ee38669d363335edeabd3eb0aaa0bc39
ArunavaCoderEm <acecankill@gmail.com>
2024-11-25 01:50:39 +0530

    getting it now nice

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,6 @@
+import { ChildrenType } from "@/Types/types";
+import React from "react";
+
+export default function layout({ children }: ChildrenType): React.ReactNode {
+  return <>{children}</>;
+}
---
+++ src/app/admin/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Admin():React.ReactNode {
+  return (
+    <div>pageAdmin</div>
+  )
+}
